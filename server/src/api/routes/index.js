1218393964996c92c54dd63f694b4955d0792c9d
Wouter Vlaeyen <woutvlae@student.arteveldehs.be>
2019-12-16 09:24:59 +0100

    Fix our own mistakes

--- server/src/api/routes/index.js
+++ server/src/api/routes/index.js
@@ -4,6 +4,7 @@ import locationRouter from './location.route';
 import tagRouter from './tag.route';
 import liveRequestRouter from './liveRequest.route';
 import userRouter from './user.route';
+import roleRouter from './role.route';
 
 const express = require('express');
 
@@ -13,6 +14,7 @@ router.use('/request', requestRouter);
 router.use('/subject', subjectRouter);
 router.use('/location', locationRouter);
 router.use('/tag', tagRouter);
+router.use('/role', roleRouter);
 router.use('/liveRequest', liveRequestRouter);
 router.use('/user', userRouter);
 
