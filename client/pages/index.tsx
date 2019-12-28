738998545561dba9d083741b397ecb651808976b
cedrvanh <cedrvanh@student.arteveldehs.be>
2019-12-28 22:52:45 +0100

    Added axios

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Head from 'next/head'
+import axios from 'axios'
 
 // Import layout
 import BaseLayout from '../layouts/base';
@@ -10,14 +11,28 @@ import PostCardList from '../components/postCardList'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import '../sass/main.scss'
 
-const HomePage = () => (
-  <div className="page homepage">
-    <Head>
-      <title>Home</title>
-      <link rel='icon' href='/favicon.ico' />
-    </Head>
-    <PostCardList />
-  </div>
-)
+const HomePage = (props) => {
+  console.log(props.name)
+  return (
+    <div className="page homepage">
+      <Head>
+        <title>Home</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <PostCardList />
+    </div>
+  )
+}
+
+HomePage.getInitialProps = async () => {
+  const res = await axios.get(`http://localhost:1234/api/v1/request`)
+  const data = await res
+
+  console.log(data)
+
+  return {
+    name: 'Test'
+  }
+}
 
 export default BaseLayout(HomePage)
