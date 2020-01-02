2673eb5a90e6569619cd92791769deb8b7db5be3
cedrvanh <cedrvanh@student.arteveldehs.be>
2020-01-02 16:43:21 +0100

    Added test POST request

--- client/components/forms/AddPostForm.tsx
+++ client/components/forms/AddPostForm.tsx
@@ -8,28 +8,38 @@ const AddRequestForm = () => {
     const [options, setOptions] = useState([])
     // Temporary hardcoded user ID
     const [currentUser, setCurrentUser] = useState('5e0a5c0fb2a65d540c6f7998')
-    const [input, setInput] = useState({})
+    const [inputs, setInputs] = useState({})
 
     useEffect(() => {
         setOptions([...options, 'Web Development II', 'Multimedia', 'Mobile Development I'])
+        setInputs({...inputs, student_id: currentUser})
     }, [])
     
     const selectedTags = (tags: String[]) => console.log(tags)
 
     const handleSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault() 
+        
+        const request = {
+            message: 'Help mij CORS',
+            student_id: '5e0a5c0fb2a65d540c6f7998',
+            subject_id: '5e0a5e2cb2a65d540c6f79a6'
+        }   
+
+        RequestService.createRequest(JSON.stringify(request))
+        RequestService.createRequest(request)
     }
     
     const handleInputChange = (e) => {
-        setInput({
-            ...input, 
+        setInputs({
+            ...inputs,
             [e.currentTarget.name]: e.currentTarget.value
         })
     }
 
     return (
         <form onSubmit={handleSubmit} className="form-container">
-            <TextArea label="Question" name="question" rows={5} onChange={handleInputChange}/>
+            <TextArea label="Message" name="message" rows={5} onChange={handleInputChange}/>
             <SelectInput placeholder="Choose subject" name="subject" options={options} onChange={handleInputChange}/>
             <TagInput name="tags" onChange={handleInputChange} selectedTags={selectedTags}/>
             <Button label="Add Question" />
--- client/services/api.tsx
+++ client/services/api.tsx
@@ -2,5 +2,9 @@ import axios from 'axios'
 
 // Default Axios Instance
 export const _axiosInstance = axios.create({
-    baseURL: 'http://localhost:1234/api/v1/'
+    baseURL: 'http://localhost:1234/api/v1/',
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+    }
 })
