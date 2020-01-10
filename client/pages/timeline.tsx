1993fc9f5ecef94edb7deb60b740e4eb68ff243c
nawatend <32384185+nawatend@users.noreply.github.com>
2020-01-10 16:36:57 +0100

    sort by Yearly

--- client/pages/timeline.tsx
+++ client/pages/timeline.tsx
@@ -1,24 +1,64 @@
-import React, { Component } from 'react'
+import React from 'react'
 // Import layout
 import baseLayout from '../layouts/base';
-
+import { RequestService, SubjectService, CommentService } from '../services'
 import Head from 'next/head'
+import RequestCardItem from "../components/requestCardItem"
 
 
-export interface TimeLinePageProps {
+const TimeLinePage = (props) => {
 
+    let newYear = null
+
+    return (
+        <div className="page profilepage" >
+            {props.requests.map((request, i) => {
+
+                const filteredComments = props.comments.filter(comment => comment.request_id === request._id)
+
+                //check for date
+                const timestamp = request._id.toString().substring(0, 8)
+                let date = new Date(parseInt(timestamp, 16) * 1000)
+                // let nextRequestCardItemDate = new Date(request[i + 1].date)
+
+                console.log(date)
+                if (i === 0) {
+                    newYear = date.getFullYear()
+                    return (
+                        <div key={request._id} className="card__line">
+                            <div className="titles__year">{date.getFullYear()}</div>
+                            <RequestCardItem key={request._id} request={request} comments={filteredComments} />
+                        </div>)
+                }
+                if (date.getFullYear() !== newYear) {
+                    newYear = date.getFullYear()
+                    return (
+                        <div key={request._id} className="card__line">
+                            <div className="titles__year">{date.getFullYear()}</div>
+                            <RequestCardItem key={request.id} request={request} />
+                        </div>
+                    )
+                } else {
+                    return (
+                        <RequestCardItem key={request._id} request={request} />
+                    )
+                }
+            })
+            }
+        </div>
+    );
 }
 
-export interface TimeLinePageState {
-    year: 2001
-}
 
-class TimeLinePage extends React.Component<TimeLinePageProps, TimeLinePageState> {
+TimeLinePage.getInitialProps = async () => {
+    let [requests, comments] = await Promise.all([
+        RequestService.getRequests(),
+        CommentService.getComments()
+    ])
+    requests = requests.reverse()
 
-    render() {
-        return (<div className="page profilepage" >
-            TIme Line</div>
-        );
+    return {
+        requests, comments
     }
 }
 
---
+++ client/utils/sort.tsx
@@ -0,0 +1,13 @@
+// Import services
+import { RequestService, SubjectService, CommentService } from '../services'
+
+let sortRequestsByDate = async (requests, sortType = "ASC") => {
+
+    requests.forEach(request => {
+        console.log(request)
+    });
+    return "bitches be crazy"
+
+}
+
+export default sortRequestsByDate
