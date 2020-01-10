d3c41d91fce68d28c2d76faac30bfa6c3f599742
Wouter Vlaeyen <woutvlae@student.arteveldehs.be>
2020-01-10 21:26:38 +0100

    Added study field to user model

--- server/src/api/models/user.model.js
+++ server/src/api/models/user.model.js
@@ -60,6 +60,7 @@ const UserSchema = new Schema({
   password: { type: String, required: true },
   score: { type: Number, min: 0 },
   profile_img: { type: String, required: true },
+  study: { type: String, required: true },
   role_id: { type: Schema.Types.ObjectId, required: true, ref: 'Role' },
   subjects: [{ type: Schema.Types.ObjectId, ref: 'Subject' }],
   badges: [{ type: Schema.Types.ObjectId, ref: 'Badge' }],
