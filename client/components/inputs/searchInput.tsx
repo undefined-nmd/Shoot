6075832b42849330175655fdc0e9c079a0aa789c
nawatend <32384185+nawatend@users.noreply.github.com>
2020-01-12 02:12:47 +0100

    live request sent

--- client/components/inputs/searchInput.tsx
+++ client/components/inputs/searchInput.tsx
@@ -5,7 +5,9 @@ const SearchInput = () => {
 
     const [searchTerm, setSearchTerm] = useState()
 
+
     const handleOnChange = (e) => {
+        e.preventDefault()
         setSearchTerm(e.target.value)
     }
 
@@ -14,9 +16,30 @@ const SearchInput = () => {
         return location.href = 'http://localhost:3000/result?searchTerm=' + searchTerm;
     }
 
+    const handleOnFocus = (e) => {
+        e.preventDefault()
+
+        let logo = document.getElementById("header-logo");
+        if (logo !== null) {
+            logo.classList.add("remove")
+        }
+
+    }
+
+
+    const handleOnBlur = (e) => {
+        e.preventDefault()
+
+        let logo = document.getElementById("header-logo");
+        if (logo !== null) {
+            logo.classList.remove("remove")
+        }
+
+    }
+
     return (
         <div className="form-control search-control">
-            <input type="text" placeholder="Search" onChange={e => handleOnChange(e)} />
+            <input type="text" placeholder="Search" onBlur={e => handleOnBlur(e)} onChange={e => handleOnChange(e)} onFocus={e => handleOnFocus(e)} />
             <div className="input__icon">
                 <div onClick={e => handleSearch(e)} className="input__icon--search">
                     <Icon name="search" />
---
+++ client/pages/live.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import Head from 'next/head'
+
+// Import layout
+import BaseLayout from '../layouts/base'
+
+// Import components
+import RequestCardList from '../components/requestCardList'
+
+// Import services
+import { LiveRequestService, SubjectService, CommentService, VoteService, AuthService } from '../services'
+
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { parseCookie } from '../utils/helper';
+
+
+const LivePage = (props) => {
+    return (
+        <div className="page homepage">
+            <Head>
+                <title>Home</title>
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
+            {props.requests &&
+                <RequestCardList
+                    requests={props.requests}
+                    comments={props.comments}
+                    upvotes={props.upvotes}
+                    user={props.user}
+                />
+            }
+        </div>
+    );
+}
+
+LivePage.getInitialProps = async (ctx) => {
+    const cookies = parseCookie(ctx)
+    const decodedToken = await AuthService.getDecodedToken(cookies.token)
+
+    let [liveRequests, subjects, comments, upvotes] = await Promise.all([
+        LiveRequestService.getLiveRequests(),
+        SubjectService.getSubjects(),
+        CommentService.getComments(),
+        VoteService.getVotesByStudent(decodedToken.id)
+    ])
+
+    liveRequests = liveRequests.reverse()
+
+    return {
+        liveRequests,
+        subjects,
+        comments,
+        upvotes,
+        user: decodedToken
+    }
+}
+
+export default BaseLayout(LivePage)
