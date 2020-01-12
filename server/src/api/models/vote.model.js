8b136cee52dccdff2751210bd2fa5c7887d8043c
Lukas Marynissen <lukas.marynissen@outlook.com>
2020-01-12 15:01:05 +0100

    vote scheme added to swagger api docs, comment scheme updated to add request_id

--- server/src/api/models/vote.model.js
+++ server/src/api/models/vote.model.js
@@ -2,6 +2,27 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Vote:
+ *      type: object
+ *      required:
+ *        - student_id
+ *        - request_id
+ *      properties:
+ *        student_id:
+ *          type: string
+ *          description: ID of sending student
+ *        request_id:
+ *          type: string
+ *          description: ID of request that is being voted on
+ *      example:
+ *        student_id: 5dad6bf132df1e35dc34b14f
+ *        request_id: 5e0a5ee6b2a65d540c6f79a8
+ */
+
 const VoteSchema = new Schema({
   student_id: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
   request_id: { type: Schema.Types.ObjectId, required: true, ref: 'Request' },
--- server/src/api/routes/vote.route.js
+++ server/src/api/routes/vote.route.js
@@ -4,10 +4,106 @@ import { VoteController } from '../controllers';
 const voteController = new VoteController();
 
 const voteRouter = express.Router();
+/**
+ * @swagger
+ * tags:
+ *  name: Votes
+ *  description: Manage votes
+ */
 
+/**
+ * @swagger
+ *
+ * /api/v1/vote:
+ *  get:
+ *    tags: [Votes]
+ *    summary: Get all votes.
+ *    produces:
+ *      - application/json
+ *    responses:
+ *      200:
+ *        description: All votes
+ *        content:
+ *          application/json:
+ *            type: array
+ *            schema:
+ *              $ref: '#/components/schemas/Vote'
+ *
+ *  post:
+ *    tags: [Votes]
+ *    summary: Create a new vote.
+ *    produces:
+ *      - application/json
+ *    responses:
+ *      200:
+ *        description: New vote created.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Vote'
+ */
 voteRouter.get('/', voteController.index)
   .post('/', voteController.create);
 
+/**
+ * @swagger
+ * /api/v1/vote/{id}:
+ *  get:
+ *    tags: [Votes]
+ *    summary: Get a vote by ID
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: ID of the vote.
+ *    responses:
+ *      200:
+ *        description: Specific vote
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Vote'
+ *  put:
+ *    tags: [Votes]
+ *    summary: Update a vote
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: ID of the vote.
+ *    responses:
+ *      200:
+ *        description: Updated vote
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Vote'
+ *  delete:
+ *    tags: [Votes]
+ *    summary: Delete a vote
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: ID of the vote.
+ *    responses:
+ *      200:
+ *        description: Removed a vote
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  description: Removed vote with id
+ */
 voteRouter.get('/:id', voteController.show)
   .put('/:id', voteController.update)
   .delete('/:id', voteController.destroy);
